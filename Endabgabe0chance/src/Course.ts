b6eb76207f5d98b3ae84c6accdb32270202eafc1
Enyalie <lightning.gj@gmail.com>
2022-06-27 22:01:36 +0200

    fortschritt

--- Endabgabe0chance/src/Course.ts
+++ Endabgabe0chance/src/Course.ts
@@ -1,14 +1,15 @@
 /// <reference path="Word.ts" />
-//import { type } from 'os';
 import * as sentenceFile from "./sentences.json";
 
-class Course{
-    sentences : SentencePair[]
+class Course {
+    sentences: SentencePair[]
 
-    quizArea : HTMLDivElement;
-    wordBlocksArea : HTMLDivElement;
-    currentSentence : number;
-    currentScore : number;
+    quizArea: HTMLDivElement;
+    wordBlocksArea: HTMLDivElement;
+    currentSentence: number;
+    currentScore: number;
+    hiddenScore: number; 
+    courseStart: 0; //Unique ID saying the maximum sentences in the Course
 
 
     /**
@@ -16,39 +17,86 @@ class Course{
      * @param quizAreaId Unique ID (including the # (e.g. #quizArea)) of the quiz area displaying 
      *                   the german sentences as well as the empty spots for spanish words
      * @param wordBlockAreaId Unique ID (including the # (e.g. #wordBlocks)) of the word block area displaying the availible spanish words
-     * @param sentencesInCourse 
+     * @param sentencesInCourse Unique ID telling how much sentences are in the Course
      */
-    public constructor(quizAreaId : string, wordBlockAreaId: string, sentencesInCourse : number){
+    public constructor(quizAreaId: string, wordBlockAreaId: string, sentencesInCourse: number) {
         // TODO Fill Sentences
-        let usedSentenceIndex : number[];
+        let usedSentenceIndex: number[];
 
-        while(sentencesInCourse > 0){
-            let sentenceIndex : number;
+        while (sentencesInCourse > this.courseStart) {
+            let sentenceIndex: number;
 
             do
                 sentenceIndex = Math.random() % sentenceFile.length;
-            while(!usedSentenceIndex.includes(sentenceIndex))
+            while (!usedSentenceIndex.includes(sentenceIndex))
 
             this.sentences.push(new SentencePair(sentenceFile[sentenceIndex].German as string[], sentenceFile[sentenceIndex].Spanish as string[]));
             usedSentenceIndex.push(sentenceIndex);
             sentencesInCourse--;
         }
 
-        
+
         this.currentScore = 0;
         this.currentSentence = 0;
+        this.hiddenScore = 0;
         this.quizArea = document.querySelector(quizAreaId) as HTMLDivElement;
         this.wordBlocksArea = document.querySelector(wordBlockAreaId) as HTMLDivElement;
     }
 
-    public startCourse(){
+    public startCourse() { //sets the Number from courseStarts and with this the Sentences via Eventlistener from the HTMLfile
+        let pickCourse: number;
+
+
+        let coursePicken1 = function () {
+            pickCourse = 5;
+            return pickCourse;
+        }
+
+        let coursePicken2 = function () {
+            pickCourse =10;
+            return pickCourse;
+        }
+
+        let coursePicken3 = function () {
+            pickCourse=15;
+            return pickCourse;
+        }
+        if (pickCourse >= 0) {
+            this.courseStart === pickCourse
+        }
+     
+
+
+        document.querySelector("#easy1").addEventListener("click", function (coursePicken1): void { });
+        document.querySelector("#medium1").addEventListener("click", function (coursePicken2): void { });
+        document.querySelector("#hard1").addEventListener("click", function (coursePicken3): void { });
 
     }
 
-    decrementScore(){
-        if(this.currentScore > 0)
+    public playCourse(){
+        while (this.courseStart>=0){
+            //vergleichen von Sätzen
+            //Erstellung eines Arrays zum vergleichen"Current Choice"-> break wenn nicht richtig über do!
+            //Dom veränderung an Playground
+            //-Eventlistener für Buttons, wo SPA Sätze enthalten sind
+            //-> werden zu current Choice bei On Click hinzugefügt
+            //Einsetzung veränderbarer an das Array angepasster DIV Elemente
+            //Funktion, welche das Array, mit den Pairs weiter gehen lässt
+            //break um das zu ermöglichen
+            //sobald Currentchoice == DESatz HiddenCounter ++
+        
+
+        }
+    }
+
+    public endCourse(){
+        if (this.hiddenScore >= this.courseStart)
+         fertig: String;
+    }
+
+    decrementScore() {
+        if (this.currentScore > 0)
             this.currentScore--;
     }
-
 
 }
